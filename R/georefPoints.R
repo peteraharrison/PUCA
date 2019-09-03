40425db322fcad8f6640a295b34f119e4560634f
Peter Harrison <P.A.Harrison@utas.edu.au>
2019-09-03 14:25:48 +1000

    Changed function to only geocode the top 20 points for a given species; minor code updates

--- R/georefPoints.R
+++ R/georefPoints.R
@@ -1,10 +1,9 @@
 #' Georeference a set of coordinates from \code{seedSource}
 #' 
-#' @description A helper function to georeference a set of coordinates from the output of \code{seedSource} using \code{geoCode}.
+#' @description A helper function to georeference the top 20 coordinates from the output of \code{seedSource} using \code{geoCode}.
 #' 
 #' @param data The output data frame of \code{seedSource}
 #' @param rank The column name used to rank the data frame
-#' @param n The number of points to georeference
 #' @param printToScreen Logic whether to print results to screen. Default is \code{TRUE}
 #' 
 #' @details The output data frame is first ranked in ascending order according to column name (i.e. "\code{climDist}"). The data frame is then reduced to \code{n} rows and georeferenced.
@@ -19,19 +18,19 @@
 #' 
 #' @export
 #' 
-georefPoints <- function(data, rank, n, printToScreen = T){
+georefPoints <- function(data, rank, printToScreen = T){
   #-- Rank data
   tmpD <- data[order(data[, rank], decreasing = F), ]
   
   #-- Georefernce the distribution points
-  tmpR <- tmpD[1:n, ]
-  geoL = list()
+  tmpR <- tmpD[1:20, ]
+  geoL <- list()
   for(g in 1:nrow(tmpR)){
-    tmpGeoRef = geoCode(paste(tmpR$Latitude[g], tmpR$Longitude[g], sep=","), type = 2)
-    tmpLoc = rbind(strsplit(tmpGeoRef, " ",)[[1]])
-    geoL[[g]] = if(length(tmpLoc) == 4){paste(tmpLoc[,1], sep="")} else{paste(tmpLoc[,1], tmpLoc[,2], sep = " ")}
+    tmpGeoRef <- PUCA::geoCode(paste(tmpR$Latitude[g], tmpR$Longitude[g], sep=","), type = 2)
+    tmpLoc <- rbind(strsplit(tmpGeoRef, " ")[[1]])
+    geoL[[g]] <- if(length(tmpLoc) == 4){paste0(tmpLoc[,1])} else{paste(tmpLoc[,1], tmpLoc[,2], sep = " ")}
   }
-  geoRefs = do.call(rbind.data.frame, geoL)
+  geoRefs <- do.call(rbind.data.frame, geoL)
   colnames(geoRefs) <- "location"
   tmpOut <- cbind(tmpR, geoRefs)
   row.names(tmpOut) <- 1:n
