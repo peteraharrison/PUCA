85f4e36c57a423b9dce8238f820a5cad96781a7b
Peter Harrison <P.A.Harrison@utas.edu.au>
2019-09-05 11:12:16 +1000

    Updated code

--- R/z.onAttach.R
+++ R/z.onAttach.R
@@ -1,3 +1,13 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to PUCA version 1.0!/nThis is a beta version, so please report errors to <P.A.Harrison@utas.edu.au>.")
+  packageStartupMessage("Welcome to PUCA version 1.0!\\nnThis is a beta version, so please report errors to <P.A.Harrison@utas.edu.au>.")
+  library(ALA4R)
+  library(FactoMineR)
+  library(mefa)
+  library(raster)
+  library(rgbif)
+  library(sp)
+  library(tcltk)
+  library(RJSONIO)
+  library(maptools)
+  library(tcltk2)
 }
